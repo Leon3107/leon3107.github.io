35e494164731bef8e98bb824dc8889c3fe4de69e
Leon Kipping <kippingleon@gmail.com>
2024-12-16 12:07:09 +0100

    Inital

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let mybutton = document.getElementById("scroll-to-top");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 24 || document.documentElement.scrollTop > 24) {
+        mybutton.style.display = "block";
+        mybutton.classList.remove("slide-out-right");
+    } else {
+        mybutton.classList.add("slide-out-right");
+    }
+	
+});
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
