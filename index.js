348a54c04ed22a1dfb825adb75ecb2b3072ad8fe
Leon Kipping <kippingleon@gmail.com>
2024-12-19 11:32:33 +0100

    Open images

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ function blurInput(){
     var menu = document.getElementById('menu');
     menu.blur();
 }
+
+function openPopUp(source, text) {
+    var popUp = document.getElementById('imagepopover');
+    var image = document.getElementById('imagelarge');
+    var imageText = document.getElementById('imagetext');
+    image.src = source;
+    imageText.innerHTML = text;
+    popUp.style.display = 'block';
+}
+function closePopUp() {
+    var popUp = document.getElementById('imagepopover');
+    var image = document.getElementById('imagelarge');
+    var imageText = document.getElementById('imagetext');
+    image.src = '';
+    imageText.innerHTML = '';
+    popUp.style.display = 'none';
+}
