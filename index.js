7af56e0f7e254b4948273398cefc22ff792269bd
Leon Kipping <kippingleon@gmail.com>
2024-12-19 16:05:54 +0100

    Revert "Blazor"
    This reverts commit d072fa9e5ca57a4496de04ff4de7b723a0262996.

---
+++ index.js
@@ -0,0 +1,20 @@
+function blurInput(){
+    var menu = document.getElementById('menu');
+    menu.blur();
+}
+
+function openPopUp(source, text) {
+    var popUp = document.getElementById('imagepopover');
+    var image = document.getElementById('imagelarge');
+    var imageText = document.getElementById('imagetext');
+    image.src = source;
+    imageText.innerHTML = text;
+}
+function closePopUp() {
+    var popUp = document.getElementById('imagepopover');
+    var image = document.getElementById('imagelarge');
+    var imageText = document.getElementById('imagetext');
+    image.src = '';
+    imageText.innerHTML = '';
+    popUp.style.display = 'none';
+}
