d97e10a4247558897ed8a8528e67a59c35d9491e
Leon Kipping <kippingleon@gmail.com>
2025-02-09 11:22:03 +0100

    New image

--- index.js
+++ index.js
@@ -2,16 +2,3 @@ function blurInput(){
     var menu = document.getElementById('menu');
     menu.blur();
 }
-
-function openPopUp(source, text) {
-    var popUp = document.getElementById('imagepopover');
-    var image = document.getElementById('imagelarge');
-    image.src = source;
-    popUp.style.display = 'unset';
-}
-function closePopUp() {
-    var popUp = document.getElementById('imagepopover');
-    var image = document.getElementById('imagelarge');
-    image.src = '';
-    popUp.style.display = 'none';
-}
