a6aa78abf34d9967c497370cc505adb60437044d
Leon <kippingleon@gmail.com>
2025-06-17 21:49:50 +0200

    Add overlay

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function blurInput(){
     var menu = document.getElementById('menu');
     menu.blur();
 }
+
+function closePopUp() {
+    var popup = document.getElementById('gallery-popover');
+    popup.classList.remove("active");
+
+}
+
+function openPopUp(source, description) {
+    var popup = document.getElementById('gallery-popover');
+    popup.classList.add("active");
+
+}
