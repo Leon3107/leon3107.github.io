f9e5edd616717516bd523bc6a97e6ab005a49db0
Leon Kipping <kippingleon@gmail.com>
2025-08-05 11:04:37 +0200

    Text

--- pages/script.js
+++ pages/script.js
@@ -15,18 +15,29 @@ function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
+  const angle = -10 * Math.PI / 180;
+  const x = canvas.width / 2;
+  const y = canvas.height - 300;
+
+
+  ctx.save(); 
+
   // Textstil
-  ctx.font = 'bold 100px Arial';
+  ctx.font = 'bold 120px Arial';
   ctx.fillStyle = 'white';
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.6)';
+  ctx.shadowBlur = 15;
+  ctx.shadowOffsetX = 4;
+  ctx.shadowOffsetY = 4;
 
-  // Position (z. B. mittig unten)
-  const x = canvas.width / 2;
-  const y = canvas.height - 150;
-  
+  ctx.translate(x, y);
+  ctx.rotate(angle);
 
   ctx.fillText(text, x, y);
+
+  ctx.restore();
 }
 
 function downloadImage() {
